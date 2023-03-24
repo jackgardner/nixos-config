e2aa6b6030281158fb37fc8c2d0d4bfa16337180
Jack Gardner <stonefeet@gmail.com>
2023-03-24 21:56:16 +0000

    initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,93 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+
+{ inputs, lib, config, pkgs, ... }: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  # TODO: Set your username
+  home = {
+    username = "jack";
+    homeDirectory = "/home/jack";
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+  };
+
+  # Add stuff for your user as you see fit:
+  programs.neovim.enable = true;
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "nix-env";
+	src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "00c6cc762427efe08ac0bd0d1b1d12048d3ca727";
+          sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
+      	};
+      }
+    ];
+  };
+  programs.tmux.enable = true;
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "Github";
+      italic-text = "always";
+    };
+  };
+  home.packages = with pkgs; [
+        kitty
+	hello
+	firefox
+        thunderbird
+        bitwarden
+        spotify 
+	steam
+	
+  ];
+ 
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  programs.git.userEmail = "me@jackg.se";
+  programs.git.userName = "Jack Gardner";
+
+  
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "22.11";
+}
